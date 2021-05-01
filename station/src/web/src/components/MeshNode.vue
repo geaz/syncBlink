52ce83d185858004054ed8c3847e436def5a8fbd
Gerrit Gazic <geaz@users.noreply.github.com>
2021-05-01 15:05:56 +0200

    Changed node visualization

--- station/src/web/src/components/MeshNode.vue
+++ station/src/web/src/components/MeshNode.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="mesh-node">      
-      <div class="node-label">Node {{nodeInfo.clientId.toString(16)}}<small>v{{nodeInfo.firmwareVersion}}</small></div>
-      <div class="node-leds">{{nodeInfo.ledCount}} LEDs</div>
+      <div class="node-header">
+        <span class="node-label">{{ nodeInfo.label ? nodeInfo.label : "(No Label)" }}</span>
+        <div>
+            <span class="node-id">ID {{nodeInfo.clientId.toString(16)}}</span>
+            <div class="node-info">
+                <span class="node-version">v{{nodeInfo.majorVersion}}.{{nodeInfo.minorVersion}}</span> / <span class="node-leds">{{nodeInfo.ledCount}} LEDs</span>
+            </div>
+        </div>
+      </div>      
       <div class="node-children">          
         <MeshNode v-for="child in nodeInfo.children" :key="child.clientId" :nodeInfo="child"></MeshNode>
       </div>
@@ -27,11 +34,20 @@
         text-align: center;
         border: 1px solid #ccc;
         border-radius: 5px;
-        padding: 10px;
+        box-shadow: 0 0px 2px rgba(0,0,0,.33);
     }
 
-    .node-label {
-        margin-right: 5px;
+    .node-header {
+        padding: 10px;
+        background: white;
+        border-radius: 5px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    .node-id, .node-info {
+        line-height: 0.75rem;
+        font-size: 0.833em;
+        color: #777777;
     }
 
     .node-children {
